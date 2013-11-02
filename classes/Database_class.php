79bf3fe246232488e65bed1b842ca7d9050b0182
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 19:09:37 -0400

    Update Database_class.php

--- classes/Database_class.php
+++ classes/Database_class.php
@@ -5,7 +5,7 @@ class Database {
 	private $connection;
     private $debug = true;
 	
-	public function __construct($host, $user, $pass, $db) {
+	public function __construct($host, $user, $pass, $db , false, "/media/sdp1/home/masterme120/private/mysql/socket") {
 		$this->lastResult = NULL;
 		$this->connection = mysqli_connect ( $host, $user, $pass, $db ) or die ( 'HOST ERROR.' );
 		mysqli_select_db ( $this->connection, $db ) or die ( 'DATABASE ERROR.' );
