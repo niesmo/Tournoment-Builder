a3612d8bcc781ab4b34b5ca090d4f215bbc53bb6
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 18:40:29 -0400

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,3 +1,18 @@
 <?
+session_start();
+function __autoload($class)
+{
+    require_once (__DIR__ . "/../classes/" . $class . "_class.php");
+}
+
+
+define ( DB_USER, "root" );
+define ( DB_PASS, "1yy5u8Uwmy1go57h" );
+define ( DB_HOST, "http://masterme120.chronos.feralhosting.com/" );
+define ( DB_DB, "Tournament" );
+
+$db = new Database ( DB_HOST, DB_USER, DB_PASS, DB_DB );
+if (!$db->isConnected())
+    die("DATABASE IS NOT CONNECTED");
 
 ?>
