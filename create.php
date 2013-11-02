dcd766e318111c810d4ab1bf1a0be6ffbf052576
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 19:52:53 -0400

    added css, spelling fix

--- create.php
+++ create.php
@@ -7,19 +7,21 @@ if($_POST[submit]){
 		"'$_POST[name]', '$_POST[type]', '$now', 'OPEN', '$_POST[description]', '$_POST[rules]'");
 }
 ?>
-<form action="#" method="POST">
+<div class="container">
+<form action="#" method="POST" class="form-signin">
   <fieldset>
-    <legend>Create Tournament</legend>
-	<input name="name" type="text" placeholder="Tournament Name"><br>
+    <h2 class="form-signin-heading">Create Tournament</h2>
+	<input class="input-block-level" name="name" type="text" placeholder="Tournament Name"><br>
 	<select name="type">
 	  <option value="SINGLE">Single Elimination</option>
 	  <option value="DOUBLE">Double Elimination</option>
 	  <option value="ELO">Elo Ranking System</option>
 	  <option value="MTG">Tournament Inspired by MtG</option>
 	</select><br>
-	<textarea name="description" rows="3" placeholder="Description"></textarea><br>
-	<textarea name="rules" rows="3" placeholder="Rules"></textarea><br>
-	<input type="submit" name="submit" value="Creat Tournament" />
+	<textarea class="input-block-level" name="description" rows="3" placeholder="Description"></textarea><br>
+	<textarea class="input-block-level" name="rules" rows="3" placeholder="Rules"></textarea><br>
+	<input type="submit" name="submit" value="Create Tournament" />
   </fieldset>
 </form>
+</div>
 <?include("inc/footer.php");?>
