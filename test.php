c5eff6aba0cac1a45d74a4c56ff7da0fbf527f22
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 18:36:01 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,62 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Tournament Generator</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-    </style>
-    <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/bootstrap-responsive.css" rel="stylesheet">
-
-    </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-            <p class="navbar-text pull-right">
-              Logged in as <a href="#" class="navbar-link">Username</a>
-            </p>
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid">
+<?require_once("http://masterme120.chronos.feralhosting.com/hackathon/dev/inc/header.php")?>
       <div class="row-fluid">
         <div class="span3">
           <div class="well sidebar-nav">
@@ -122,31 +64,4 @@
           </div><!--/row-->
         </div><!--/span-->
       </div><!--/row-->
-
-      <hr>
-
-      <footer>
-        <p>&copy; Company 2013</p>
-      </footer>
-
-    </div><!--/.fluid-container-->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/jquery.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-transition.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-alert.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-modal.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-dropdown.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-scrollspy.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-tab.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-tooltip.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-popover.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-button.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-collapse.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-carousel.js"></script>
-    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-typeahead.js"></script>
-
-  </body>
-</html>
+<?require_once("http://masterme120.chronos.feralhosting.com/hackathon/dev/inc/footer.php")?>
