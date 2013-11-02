8786c2cf7adf69e1724c4374de320295af87c737
BenjaminFair <fair.105@osu.edu>
2013-11-02 18:53:00 -0400

    Added database connection to create.php

--- create.php
+++ create.php
@@ -1,6 +1,15 @@
 <? include("inc/header.php");
 if($_POST[submit]){
-	print_r($_POST);	
+	print_r($_POST);
+	$con=mysqli_connect("localhost:/media/sdp1/home/masterme120/private/mysql/socket", "root",
+		"1yy5u8Uwmy1go57h", "Tournament");
+	if(mysqli_connect_errno($con)) {
+		echo "Connection to MySQL database failed: " . mysqli_connect_error();
+	}// else {
+		//mysqli_query($con, "INSERT INTO Tournament (Name, Type, StartDate, Status, Description,
+		//	Rules) VALUES ('$_POST[name]', '$_POST[type]', 'NOW()', 'OPEN',
+		//	'$_POST[description]'
+	
 }
 ?>
 <form action="#" method="POST">
@@ -14,6 +23,7 @@ if($_POST[submit]){
 	  <option value="MTG">Tournament Inspired by MtG</option>
 	</select>
 	<textarea name="description" rows="3"></textarea>
+	<textarea name="rules" rows="3"></textarea>
 	<input type="submit" name="submit" value="Creat Tournament" />
   </fieldset>
 </form>
