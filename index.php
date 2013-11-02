e68870bb9085506779aea09fd59b1e08da57e010
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 17:26:43 -0400

    setup base homepage

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
 <? require_once (inc/header.php);
 ?>
+    <div class="container-narrow">
 
-<div>Hello, world!</div>
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h3 class="muted">Tournament Builder</h3>
+      </div>
+            <div class="jumbotron">
+        <h1>Create, Join and Manage Tournaments!</h1>
+        <p class="lead">Tournament Builder lets you build Coowesomazing tournaments easily</p>
+        <a class="btn btn-large btn-success" href="#">Create a tournament</a>
+      </div>
+      <hr>
+
+      <div class="row-fluid marketing">
+        <div class="span6">
+          <h4>Tournament Name1</h4>
+          <p>Tournament Description here.</p>
+
+          <h4>Tournament Name2</h4>
+          <p>Tournament Description here.</p>
+
+          <h4>Tournament Name3</h4>
+          <p>Tournament Description here.</p>
+
+        </div>
+
+        <div class="span6">
+          <h4>Tournament Name4</h4>
+          <p>Tournament Description here.</p>
+
+          <h4>Tournament Name5</h4>
+          <p>Tournament Description here.</p>
+
+          <h4>Tournament Name6</h4>
+          <p>Tournament Description here.</p>
+        </div>
+      </div>
+
 
 <? require_once (inc/footer.php);
 ?>
