80a0f44fa50c4384f5d7425f46587a422939bac7
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 18:34:38 -0400

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,27 @@
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2013</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/jquery.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-transition.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-alert.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-modal.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-dropdown.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-scrollspy.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-tab.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-tooltip.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-popover.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-button.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-collapse.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-carousel.js"></script>
+    <script src="http://masterme120.chronos.feralhosting.com/hackathon/dev/js/bootstrap-typeahead.js"></script>
+
   </body>
 </html>
