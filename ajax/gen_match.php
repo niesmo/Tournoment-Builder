9f543553be3982260d843c2e205cf4bd4cd606bf
BenjaminFair <fair.105@osu.edu>
2013-11-03 01:51:49 -0500

    debug 3

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -2,16 +2,4 @@
 if(isset($_GET['round']) && isset($_GET['id'])) {
 	echo "found data from request";
 } else echo "round and/or id not found in request!";
-
-private function merge($a, $b) {
-	$out = [];
-	foreach($a as $array) foreach($array as $key => $value)	$out[] = $value;
-	foreach($b as $array) foreach($array as $key => $value)	$out[] = $value;
-	sort($out);
-	return $out;
-}
-
-private function gen_matches($round, $entryIDs) {
-	print_r($entryIDs);
-}
 ?>
