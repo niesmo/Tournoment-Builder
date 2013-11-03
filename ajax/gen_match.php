820eeb38a416a9765e564fa512a298ac91caf416
BenjaminFair <fair.105@osu.edu>
2013-11-03 02:14:14 -0500

    close parenthesis

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -9,7 +9,7 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
 		e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
 		m.Result != 'null'", "MatchID")[0][0] == 0) { // all results in from previous round
-			if($round >= ceil(log(count($initialEntries, 2))) // final round
+			if($round >= ceil(log(count($initialEntries, 2)))) // final round
 				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
 			else {
 				$first = db->select("`Match` as m , Entry as e", "EntryID1",
