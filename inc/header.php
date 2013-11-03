04ecd6edd7710aa760a29ae4e2e09fcd35f74e94
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 01:16:20 -0400

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -8,7 +8,6 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    custom.css
     <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/custom.css" rel="stylesheet">
     <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
