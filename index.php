0700d2a46331cad51ea329141c23470ed34dc990
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 20:18:41 -0400

    asdf

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
       for ($x=0; $x<9; $x++)
       {
         if ((($x+1)%3)==0) {
-          echo "<div class='row-fluid'>";
+          echo "<div class='row-fluid'>\n";
         }
-        echo "<div class='span4'>";
-        echo "<h4><a href=\"view_tournament.php?". $list_of_tournaments[$x][id]. "\">". $list_of_tournaments[$x][Name]." </a></h4>";
-        echo "<p>Tournament Description:". $list_of_tournaments[$x][Description] ." </p>";
-        echo "<p><a class='btn' href='#'>View details &raquo;</a></p>";
-        echo "</div>";
+        echo "<div class='span4'>\n";
+        echo "<h4><a href=\"view_tournament.php?". $list_of_tournaments[$x][ID]. "\">". $list_of_tournaments[$x][Name]." </a></h4>\n";
+        echo "<p>Tournament Description:". $list_of_tournaments[$x][Description] ." </p>\n";
+        echo "<p><a class='btn' href='#'>View details &raquo;</a></p>\n";
+        echo "</div>\n";
         if ((($x+1)%3)==0) {
           echo "</div> <!--/row-->";
         }
