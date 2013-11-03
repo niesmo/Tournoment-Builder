08b674ef49299cb28008911d63468644a48ed944
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 08:27:09 -0500

    Update Participant_class.php

--- classes/Participant_class.php
+++ classes/Participant_class.php
@@ -3,17 +3,18 @@ class Participant{
         private $db;
         public function __construct( $db ){
                 $this->db = $db;
-                echo "Participant object Created";
+                //echo "Participant object Created";
         
         }
         public function getParticipantInfo($entry_id , $fields = "*"){
-                echo "we are in the info";
+                //echo "we are in the info";
                 if($entry_id == -1){
                         return  array("Name" => "Bye");
                 }
-                print_r($this->db->select("Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.EntryID = $entry_id"));
-                return $this->db->select("Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.EntryID = $entry_id")[0];
-        }
+                //print_r($this->db->select("Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.EntryID = $entry_id"));
+                $data = $this->db->select("Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.EntryID = $entry_id");
+                return $data[0];
+        }       
         
         public function removeParticipant($participant_id){
                 return $this->db->delete("Participant",  "ParticipantID = $participant_id");
