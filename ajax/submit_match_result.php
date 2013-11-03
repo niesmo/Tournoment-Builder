005ef410437fa684e36f6c9c0a3446abea577d6a
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 01:40:03 -0500

    Update submit_match_result.php

--- ajax/submit_match_result.php
+++ ajax/submit_match_result.php
@@ -2,9 +2,9 @@
 //print_r($_POST);
 if(isset($_POST['result']) && isset($_POST['MatchID'])) {
 	print_r($_POST);
-	//if($db->update("`Match`", "Result = '$_POST[result]'", " MatchID = $_POST[MatchID]") == 1)
-	 // echo "SUCCESS";
-	//else
-	 // echo "FAILED";
+	if($db->update("`Match`", "Result = '$_POST[result]'", " MatchID = $_POST[MatchID]") == 1)
+	 	echo "SUCCESS";
+	else
+	 	echo "FAILED";
 }
 ?>
