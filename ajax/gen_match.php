d509ef38f63e3db758d2288f26b0b1c036bbf198
BenjaminFair <fair.105@osu.edu>
2013-11-03 01:20:03 -0400

    last asynchronous match generation

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,19 +1,26 @@
 <? include('../conf/config.php');
-if(isset($_POST['order']) && isset($_POST['round']) && isset($_POST['EntryID']) &&
-	isset($_POST['TournamentID'])) {
-	$totalEntries = $db->select("`Match`", "COUNT(*)", "Round='$_POST[round]'")[0][0];
-	if($_POST['round'] < log($totalEntries, 2)) { // not final round
+if(isset($_GET['order']) && isset($_GET['round']) && isset($_GET['EntryID']) &&
+	isset($_GET['TournamentID'])) {
+	$totalEntries = $db->select("Entry", "COUNT(*)")[0][0];
+	if($_GET['round'] < log($totalEntries, 2)) { // not final round
 		$sister = db->select("`Match`", "EntryID1, EntryID2, Result", 
-			"Round='$_POST[round]' AND `Order`=('_POST[order]'%2 == 0 ? " . // for even, go up one
-			"_POST[order]+1 : _POST[order]-1)", "", "", "1"); // for odd, go down one
+			"Round='$_GET[round]' AND `Order`=('_GET[order]'%2 == 0 ? " . // for even, go up one
+			"_GET[order]+1 : _GET[order]-1)", "", "", "1"); // for odd, go down one
 	
 		if(!isempty($sister) && $sister['Result'] != "") { // if sister match has a result
 			$sisterWinner = $sister[$sister['Result'] == "FIRST" ? 'EntryID1' : 'EntryID2'];
-			db->insert("`Match`", "EntryID1, EntryID2, `Order`, Round", min($_POST['EntryID'],
-			$sisterWinner) . ", " . max($_POST['EntryID'], $sisterWinner) . // sort entries
-			", $_POST['order'] DIV 2, $_POST['round']+1"); // calc next order and round #s
+			db->insert("`Match`", "EntryID1, EntryID2, `Order`, Round", min($_GET['EntryID'],
+			$sisterWinner) . ", " . max($_GET['EntryID'], $sisterWinner) . // sort entries
+			", $_GET['order'] DIV 2, $_GET['round']+1"); // calc next order and round #s
 		}
 	} else { // final round
-		db->update("Tournament", "Status='CLOSE'", "TournamentID='$_POST[TournamentID]'");
+		db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[TournamentID]'");
 	}
-} ?>
+} elseif($_GET['round'] == '0' && isset($_GET['TournamentID']) { // first round: gen all matches
+	$totalEntries = $db->select("Entry", "COUNT(*)")[0][0];
+	$bye = $totalEntries
+	for($i=0;$i<floor($totalEntries/2);$i++) {
+		
+	
+	
+?>
