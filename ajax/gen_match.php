6b2a26fafd85f3fbbc365c7360e7f60a6fcf6e6d
BenjaminFair <fair.105@osu.edu>
2013-11-03 02:12:19 -0500

    test fix

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -2,7 +2,6 @@
 if(isset($_GET['round']) && isset($_GET['id'])) {
 	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
 	$initialEntries = merge($initialEntries, []);
-	print_r($initialEntries);
 	if($_GET['round'] == '0') { // first round: gen from Entry
 		gen_matches(0, $initialEntries);
 	} else {
@@ -10,7 +9,19 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
 		e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
 		m.Result != 'null'", "MatchID")[0][0] == 0) { // all results in from previous round
-
+			if($round >= ceil(log(count($initialEntries, 2))) // final round
+				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
+			else {
+				$first = db->select("`Match` as m , Entry as e", "EntryID1",
+				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				m.Result = 'FIRST'", "MatchID");
+				$second = db->select("`Match` as m , Entry as e", "EntryID2",
+				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				m.Result = 'SECOND'", "MatchID");
+				gen_matches($_GET['round'], merge($first, $second)));
+			}
 		}
 	}
 } else echo "round and/or id not found in request!";
