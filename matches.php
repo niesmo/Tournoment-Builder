8c0f3169dd10d13cf3dc50e1b40d611ff9730f75
BenjaminFair <fair.105@osu.edu>
2013-11-02 23:00:24 -0400

    fix unmarked PHP in matches.php

--- matches.php
+++ matches.php
@@ -7,8 +7,36 @@ if(!isset($_GET["id"])) {
 		"TournamentID = $_GET[id]")[0]; // get name and description
 	echo "<h1>Current Matches for " . $result["Name"] . "</h1>";
 	echo "<p>" . $result["Description"] . "</p>"; ?>
-
+<table>
+	<thead>
+		<tr>
+			<th>Entrant 1</th>
+			<th>Entrant 2</th>
+			<th>Winner</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
 <? 	$matches = $db->select("`Match` as m , Entry as e", "EntryID1, EntryID2, Result",
-	"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = 2");
-?>
+	"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = 2",
+	"MatchID");
+foreach($matches as $match) {?>
+		<tr>
+		<?if($match["Result"] == "") { // match in progress	
+			<td><button class="btn"><?echo $match["EntryID1"]?></button></td>
+			<td><button class="btn"><?echo $match["EntryID2"]?></button></td>
+			<td><i>Awaiting Results</i></td>
+			<td><button class="btn">Draw</button>
+		<?} else {?>
+			<td><?echo $match["EntryID1"]?></td>
+			<td><?echo $match["EntryID2"]?></td>
+			<td><?	if($match["Result"] == "FIRST") echo $match["EntryID1"];
+					elseif($match["Result"] == "SECOND") echo $match["EntryID2"];
+					elseif($match["Result"] == "TIE") echo "Draw";?></td>
+			<td></td>
+		<?}?>
+		</tr>
+<?} // end foreach ?>
+	</tbody>
+</table>
 <?} include("inc/footer.php"); ?>
