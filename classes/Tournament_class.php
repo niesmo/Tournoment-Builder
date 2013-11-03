89e9d881bc832db541d33203f472cfa37f136dfa
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 09:21:58 -0500

    Update Tournament_class.php

--- classes/Tournament_class.php
+++ classes/Tournament_class.php
@@ -31,6 +31,7 @@ class Tournament{
 				$numberOfPartsNeeded = pow(2,ceil(log(count($participants) , 2)));
 				$count = ($numberOfPartsNeeded-1);
 				for($i=($numberOfPartsNeeded-1);$i>=0;$i--){
+					echo $count ."<br>";
 					if($i > count($participants)-1){
 						$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , -1 , 1 , 'FIRST' , 0");
 					}
