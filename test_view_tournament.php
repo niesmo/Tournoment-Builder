d869d81d5375bcf1e6844b73f87f6116b2eb91dd
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-03 11:44:38 -0500

    asdf

--- test_view_tournament.php
+++ test_view_tournament.php
@@ -28,14 +28,14 @@ var num_buckets = Math.pow(2,(Math.ceil(Math.log(num_participants)/Math.LN2)));
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = num_buckets*200;
+ctx.canvas.height = window.innerHeight;
 
 ctx.font="40px Arial";
 ctx.fillText("<? echo $result['Name']; ?>",window.innerWidth/2,50);
 
 var players = new Array();
 
-var player_height = ctx.canvas.height/num_buckets;
+var player_height = window.innerHeight/num_buckets;
 var player_width = 150;
 	
 	<? foreach($participants as $val){
