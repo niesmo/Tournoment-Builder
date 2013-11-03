9ffe26b6d44a79267aff3787c321aa8adbf5141a
BenjaminFair <fair.105@osu.edu>
2013-11-03 01:45:16 -0500

    more gen_match.php

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,28 +1,35 @@
 <? include('../conf/config.php');
-if(isset($_GET['round']) && isset($_GET['TournamentID'])) {
-	$initialEntries = $db->select("Entry", "COUNT(*)", "TournamentID=$_GET[TournamentID]")[0][0];
+if(isset($_GET['round']) && isset($_GET['id'])) {
+	$initialEntries = $db->select("Entry", "COUNT(*)", "TournamentID=$_GET[id]")[0][0];
 	if($_GET['round'] == '0') { // first round: gen from Entry
-		gen_matches(0, $initialEntries);
+		gen_matches(0, merge($initialEntries, []));
 	} else {
 		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-		e.TournamentID = '$_GET[TournamentID]' AND m.Round = $_GET[round]-1 AND 
+		e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
 		m.Result != 'null'", "MatchID")[0][0] == 0) { // all results in from previous round
 			if($round >= ceil(log(count($initialEntries, 2))) // final round
-				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[TournamentID]'");
+				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
 			else {
 				$first = db->select("`Match` as m , Entry as e", "EntryID1",
 				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-				e.TournamentID = '$_GET[TournamentID]' AND m.Round = $_GET[round]-1 AND
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
 				m.Result = 'FIRST'", "MatchID");
 				$second = db->select("`Match` as m , Entry as e", "EntryID2",
 				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-				e.TournamentID = '$_GET[TournamentID]' AND m.Round = $_GET[round]-1 AND
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
 				m.Result = 'SECOND'", "MatchID");
-				gen_matches($_GET['round'], sort(array_merge($first, $second)));
+				gen_matches($_GET['round'], merge($first, $second)));
 			}
 		}
 	}
+}
+private function merge($a, $b) {
+	$out = [];
+	foreach($a as $array) foreach($array as $key => $value)	$out[] = $value;
+	foreach($b as $array) foreach($array as $key => $value)	$out[] = $value;
+	sort($out);
+	return $out;
 }
 
 private function gen_matches($round, $entryIDs) {
