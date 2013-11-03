ca3e8a13ba540e486e3e760252acaabb8ae8042e
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 21:30:38 -0400

    asdf

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ if(!isset($_GET[id])) {
 	echo "<p>" . $result[Description] . "</p>"; // description
 	echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
 
-	$participants =$tournament->getparticipants($_GET[id]);
+	$participants =$tournament->getParticipants($_GET[id]);
 	print_r($participants);
 
 	echo "<h3>Current Participants:</h3><p>" . $participants[] . "</p>";
