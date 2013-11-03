3d1c21d4b92ece0b52d2474ac1060e899301fafe
BenjaminFair <fair.105@osu.edu>
2013-11-03 02:10:44 -0500

    debug 3

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,9 +1,10 @@
 <? include('../conf/config.php');
 if(isset($_GET['round']) && isset($_GET['id'])) {
-	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]")[0][0];
+	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
+	$initialEntries = merge($initialEntries, []);
 	print_r($initialEntries);
 	if($_GET['round'] == '0') { // first round: gen from Entry
-		gen_matches(0, merge($initialEntries, []));
+		gen_matches(0, $initialEntries);
 	} else {
 		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
