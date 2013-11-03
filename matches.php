95865ff5acd49a861ee5fd9e9f215a1c6f617642
BenjaminFair <fair.105@osu.edu>
2013-11-02 22:52:09 -0400

    debug 7

--- matches.php
+++ matches.php
@@ -8,4 +8,7 @@ if(!isset($_GET["id"])) {
 	echo "<h1>Current Matches for " . $result["Name"] . "</h1>";
 	echo "<p>" . $result["Description"] . "</p>"; ?>
 
+<? 	$matches = $db->select("`Match` as m , Entry as e", "EntryID1, EntryID2, Result",
+	"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = 2");
+?>
 <?} include("inc/footer.php"); ?>
