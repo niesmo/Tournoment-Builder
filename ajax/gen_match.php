752b72c90dcf4c71a8d4e933f6ca5c9fb3612853
BenjaminFair <fair.105@osu.edu>
2013-11-03 03:07:33 -0500

    global db

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,9 +1,10 @@
 <? include('../conf/config.php');
+global $db;
 if(isset($_GET['round']) && isset($_GET['id'])) {
 	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
 	$initialEntries = merge($initialEntries, []);
 	if($_GET['round'] == '0') { // first round: gen from Entry
-		gen_matches(0, $initialEntries, $db);
+		gen_matches(0, $initialEntries);
 	} else {
 		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
@@ -20,7 +21,7 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
 				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
 				m.Result = 'SECOND'", "MatchID");
-				gen_matches($_GET['round'], merge($first, $second) , $db);
+				gen_matches($_GET['round'], merge($first, $second) );
 			}
 		}
 	}
@@ -34,9 +35,8 @@ function merge($a, $b) {
 	return $out;
 }
 
-function gen_matches($round, $entryIDs, $db) {
-	if($db->isConnected())
-		echo "<h1>Connected</h1>";
+function gen_matches($round, $entryIDs) {
+	
 	print_r($entryIDs);
 	for($i=0;$i<count($entryIDs)/2;$i++) {
 		$db->insert("`Match`", "EntryID1, EntryID2, Round, Order",
