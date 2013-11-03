f635d96c5f1a97e8592692e43010ba90a43b59ef
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-03 02:05:25 -0500

    asdf

--- edit_tournament.php
+++ edit_tournament.php
@@ -6,31 +6,26 @@ if(!isset($_GET['id']) && !isset($_POST['id'])) {
 
         $t_id = (isset($_GET['id']))?$_GET['id']:$_POST['id'];
         if(isset($_POST['submit'])){
-                $db->update("Tournament", "Name='$_POST[tournament_name]', Rules='$_POST[rules]', Description='$_POST[description]' , Status='$_POST[status]' , Type='$_POST[type]'","TournamentID=$t_id");
+                $db->update("Tournament", "Name='$_POST[tournament_name]', Description='$_POST[description]'","TournamentID=$t_id");
                 
         }?>
       <div class="row-fluid">
         <div class="span6">
         <?
-        $result = $db->select("Tournament", "Name, Description, Rules, Status, Type",
+        $result = $db->select("Tournament", "Name, Description, Rules",
                 "TournamentID = $t_id")[0]; // get name, description, and rules
         echo "<h1>" . $result['Name'] . "</h1>"; // name
         echo "<p>" . $result['Description'] . "</p>"; // description
-        echo "<h3>Rules:</h3><p>"."<h4>". $result['Type'] ."</h4>"  . $result['Rules'] . "</p>";
-		echo "<h3>Status:" . $result['Status'] . "</h3>";
+        echo "<h3>Rules:</h3><p>" . $result['Rules'] . "</p>";
 
-		
         $participants =$tournament->getParticipants($t_id);
         // print_r($participants);
         if(count($participants)>0){
                 echo "<h3>Current Participants:</h3><ol>";
                 foreach($participants as $val){
-
                         echo "<li>";
                         echo $val['Name'];
                         echo "</li>\n";
-                        echo "<form action='remove_player.php' method='POST'><input class='btn btn-danger' type='submit' name='submit' value='Remove $val['Name']' /></br></form>";
-                        
                 }        
         }
         else{
@@ -45,9 +40,9 @@ if(!isset($_GET['id']) && !isset($_POST['id'])) {
 <form action="register.php" method="POST">
   <fieldset>
     <h2 class="form-signin-heading">Add Player</h2>
-   	<input type="hidden" name="id" value="<? echo $t_id; ?>"/>
-	<input name="name" type="text" placeholder="Participant Name"/>
-	<input class="btn btn-primary btn-large" type="submit" name="submit" value="Add Player" />
+           <input type="hidden" name="id" value="<? echo $t_id; ?>"/>
+        <input name="name" type="text" placeholder="Participant Name"/>
+        <input class="btn btn-primary btn-large" type="submit" name="submit" value="Add Player" />
   </fieldset>
 </form>
 </div>
@@ -55,23 +50,17 @@ if(!isset($_GET['id']) && !isset($_POST['id'])) {
 <form action="edit_tournament.php" method="POST">
   <fieldset>
     <h2 class="form-signin-heading"><? echo $result['Name']; ?></h2>
-   	<input type="hidden" name="id" value="<? echo $t_id; ?>"/>
-	<input name="tournament_name" type="text" value="<? echo $result['Name']; ?>"/>
-	<select name="type">
-	  <option value="SINGLE" <? if($result['Type']=='SINGLE'){echo 'selected';} ?>>Single Elimination</option>
-	  <option value="DOUBLE" <? if($result['Type']=='DOUBLE'){echo 'selected';} ?>>Double Elimination</option>
-	  <option value="ELO" <? if($result['Type']=='ELO'){echo 'selected';} ?>>Elo Ranking System</option>
-	  <option value="MTG" <? if($result['Type']=='MTG'){echo 'selected';} ?>>Tournament Inspired by MtG</option>
-	</select>
-	<select name="status">
-	  <option value="OPEN" <? if($result['Status']=='OPEN'){echo 'selected';} ?>>Open</option>
-	  <option value="PROGRESS" <? if($result['Status']=='PROGRESS'){echo 'selected';} ?>>In-Progress</option>
-	  <option value="ClOSE" <? if($result['Status']=='CLOSE'){echo 'selected';} ?>>Closed</option>
-	</select>
-	<br>
-	<textarea class="input-block-level" name="description" rows="3" value="<? echo $result['Description'] ?>"></textarea><br>
-	<textarea class="input-block-level" name="rules" rows="3" value="<? echo $result['Rules'] ?>"></textarea><br>
-	<input class="btn btn-primary btn-large" type="submit" name="submit" value="Edit Tournament Details" />
+           <input type="hidden" name="id" value="<? echo $t_id; ?>"/>
+        <input name="tournament_name" type="text" placeholder="<? echo $result['Name']; ?>"/>
+        <select name="type">
+          <option value="SINGLE">Single Elimination</option>
+          <option value="DOUBLE">Double Elimination</option>
+          <option value="ELO">Elo Ranking System</option>
+          <option value="MTG">Tournament Inspired by MtG</option>
+        </select><br>
+        <textarea class="input-block-level" name="description" rows="3" placeholder="<? echo $result['Description'] ?>"></textarea><br>
+                                <textarea class="input-block-level" name="rules" rows="3" placeholder="<? echo $result['Rules'] ?>"></textarea><br>
+        <input class="btn btn-primary btn-large" type="submit" name="submit" value="Edit Tournament Details" />
   </fieldset>
 </form>
 
