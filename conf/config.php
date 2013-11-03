ea257b2749ab6540836282065fce01e7c9ccee4e
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 21:49:29 -0400

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -11,12 +11,12 @@ function __autoload($class)
 require_once(__DIR__ . "/../inc/functions.php");
 
 
-define ( DB_USER, "root" );
-define ( DB_PASS, "1yy5u8Uwmy1go57h" );
-define ( DB_HOST, "localhost" );
-define ( DB_DB, "Tournament" );
+define ( "DB_USER", "root" );
+define ( "DB_PASS", "1yy5u8Uwmy1go57h" );
+define ( "DB_HOST", "localhost" );
+define ( "DB_DB", "Tournament" );
 
-$db = new Database ( DB_HOST, DB_USER, DB_PASS, DB_DB );
+$db = new Database ( ${DB_HOST}, ${DB_USER}, ${DB_PASS}, ${DB_DB} );
 if (!$db->isConnected())
     die("DATABASE IS NOT CONNECTED");
 //else
