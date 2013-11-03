608d804f90378a14efde9ecb8be6085a09140380
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 21:50:45 -0400

    asdf

--- register.php
+++ register.php
@@ -4,20 +4,27 @@ include("inc/header.php");
         <div class="span6">
 
 if(!isset($_GET[id])) {
-        echo "Error: No tournament ID supplied";
+	echo "Error: No tournament ID supplied";
 } else {
-        if($_POST[submit]){
-                $db->insert("Participant", "Name", "'$_POST[name]'");
-                $id = $db->lastInsertedId(); // get ParticipantID
-                echo "Test $id <br>";
-                $db->insert("Entry", "TournamentID, ParticipantID", "'$_GET[id]', '$id'");
-        }
-        $result = $db->select("Tournament", "Name, Description, Rules",
-                "TournamentID = $_GET[id]")[0]; // get name, description, and rules
-        echo "<h1>Register for " . $result[Name] . "</h1>"; // name
-        echo "<p>" . $result[Description] . "</p>"; // description
-        echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
+	if($_POST[submit]){
+		$db->insert("Participant", "Name", "'$_POST[name]'");
+		$id = $db->lastInsertedId(); // get ParticipantID
+		$db->insert("Entry", "TournamentID, ParticipantID", "$_GET[id], $id");
+	}
+	$result = $db->select("Tournament", "Name, Description, Rules",
+		"TournamentID = $_GET[id]")[0]; // get name, description, and rules
+	echo "<h1>Register for " . $result[Name] . "</h1>"; // name
+	echo "<p>" . $result[Description] . "</p>"; // description
+	echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
+
+	// $participants =$tournament->getParticipants($_GET[id]);
+	// print_r($participants);
+
+	echo "<h3>Current Participants:</h3>";
+}
 ?>
+
+
 
 <form action="#" method="POST">
   <fieldset>
