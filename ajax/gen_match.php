76b4efe6a09b71b98763a42b58f9c4e60a80a23b
BenjaminFair <fair.105@osu.edu>
2013-11-03 07:54:55 -0500

    change to POST

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,35 +1,35 @@
 <? include('../conf/config.php');
-if(isset($_GET['MatchID']) && isset($_GET['id'])) {
-	$match = $db->select("`Match`", "Round, EntryID1", "MatchID='$_GET[MatchID]'")[0];
+if(isset($_POST['MatchID']) && isset($_POST['id'])) {
+	$match = $db->select("`Match`", "Round, EntryID1", "MatchID='$_POST[MatchID]'")[0];
 	$round = $match['Round']+1;
-	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
+	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_POST[id]");
 	$initialEntries = merge($initialEntries, []);
 	if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 	"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-	AND e.TournamentID = '$_GET[id]' AND m.Round = $round-1 AND 
+	AND e.TournamentID = '$_POST[id]' AND m.Round = $round-1 AND 
 	m.Result IS NULL")[0]['COUNT(*)'] == 0) { // all results in from previous round
 		if($round >= ceil(log(count($initialEntries), 2))) {// final round
 			$lastMatch = $db->select("`Match` as m , Entry as e", "EntryID1, EntryID2, Result",
 				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-				e.TournamentID = '$_GET[id]' AND m.Round = $round-1")[0];
+				e.TournamentID = '$_POST[id]' AND m.Round = $round-1")[0];
 			print_r($lastMatch);
 			$winnerID = $lastMatch[$lastMatch['Result'] == 'FIRST' ? 'EntryID1' : 'EntryID2'];
 			$winner = $participant->getParticipantInfo($winnerID)['Name'];
-			$db->update("Tournament", "Status='CLOSE', Winner='$winner'", "TournamentID='$_GET[id]'");
+			$db->update("Tournament", "Status='CLOSE', Winner='$winner'", "TournamentID='$_POST[id]'");
 		} else {
 			$first = $db->select("`Match` as m , Entry as e", "EntryID1",
 			"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-			AND e.TournamentID = '$_GET[id]' AND m.Round = $round-1 AND
+			AND e.TournamentID = '$_POST[id]' AND m.Round = $round-1 AND
 			m.Result = 'FIRST'", "MatchID");
 			$second = $db->select("`Match` as m , Entry as e", "EntryID2",
 			"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-			AND e.TournamentID = '$_GET[id]' AND m.Round = $round-1 AND
+			AND e.TournamentID = '$_POST[id]' AND m.Round = $round-1 AND
 			m.Result = 'SECOND'", "MatchID");
 			gen_matches($round, merge($first, $second), $db );
 		}
 	}
-} elseif(isset($_GET['id'])) { // first round
-	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
+} elseif(isset($_POST['id'])) { // first round
+	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_POST[id]");
 	$initialEntries = merge($initialEntries, []);
 	$byes = pow(2, ceil(log(count($initialEntries), 2)))-count($initialEntries);
 	for($i=0;$i<$byes;$i++) { // award byes
