8a1be57a534de0d0b8cd0dbc44a109834c5ee9c3
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-03 10:19:40 -0500

    view test

--- test_view_tournament.php
+++ test_view_tournament.php
@@ -35,7 +35,7 @@ ctx.fillText("<? echo $result['Name']; ?>",window.innerWidth/2,50);
 
 var players = new Array();
 
-var player_height = ctx.canvas.height/6;
+var player_height = ctx.canvas.height/12;
 var player_width = 150;
 	
 	<? foreach($participants as $val){
@@ -46,7 +46,7 @@ var player_width = 150;
 function drawLines(num_row,column){
 	var start_height = player_height * (column/2);
 	if(num_row==1){
-		ctx.fillText("Winner",player_width * column,start_height);
+		ctx.fillText("Winner",player_width * column -150,start_height);
 		ctx.moveTo(player_width * column, start_height);
 		ctx.lineTo(player_width * (column +1),start_height);
 		ctx.strokeStyle = '#ff0000';
@@ -55,19 +55,19 @@ function drawLines(num_row,column){
 	else{
 		for(var i=0;i<num_row;i++){
 			
-			ctx.moveTo(player_width * column,start_height + (player_height * i) +5);
-			ctx.lineTo(player_width * (column+1),start_height + (player_height * i)+5);
+			ctx.moveTo(player_width * column-150,start_height + (player_height * i) +5);
+			ctx.lineTo(player_width * (column+1)-150,start_height + (player_height * i)+5);
 			ctx.stroke();
 			//diaganol lines
 			if (i%2==0) {
 				ctx.strokeStyle = '#000000';
 				ctx.fillText(players[i*column],player_width * column,start_height + (player_height * i) +5);	
-				ctx.lineTo(player_width * (column+2),start_height + (player_height * (i+.5)));
+				ctx.lineTo(player_width * (column+2)-150,start_height + (player_height * (i+.5)));
 				ctx.stroke();	
 			}else{
 				ctx.strokeStyle = '#ff0000';
 				ctx.fillText(players[i*column],player_width * column,start_height + (player_height * i) +5);	
-				ctx.lineTo(player_width * (column+2),start_height + (player_height * (i-.5)));
+				ctx.lineTo(player_width * (column+2)-150,start_height + (player_height * (i-.5)));
 				ctx.stroke();	
 			}
 		}
