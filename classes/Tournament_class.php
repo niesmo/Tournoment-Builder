b7626bfed47d4eedadef0819aa752f58b0e089ba
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 11:44:50 -0500

    Update Tournament_class.php

--- classes/Tournament_class.php
+++ classes/Tournament_class.php
@@ -97,7 +97,7 @@ class Tournament{
         	$first = $this->db->select("Entry as e, `Match` as m" , "*" , "e.EntryID = m.EntryID1  AND e.TournamentID = $t_id AND Result = 'FIRST' AND Round = $round GROUP BY MatchID") ;
         	$second = $this->db->select("Entry as e, `Match` as m" , "*" , "e.EntryID = m.EntryID2  AND e.TournamentID = $t_id AND Result = 'SECOND' AND Round = $round GROUP BY MatchID") ;
         	$total = array_merge($first, $second);
-        	print_r($total);
+        	//print_r($total);
         	return $total;
         }
         
