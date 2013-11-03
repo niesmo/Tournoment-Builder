ae008b59df602ca7718a1d977632a80c5945bd05
BenjaminFair <fair.105@osu.edu>
2013-11-03 06:22:06 -0500

    use only MatchID

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,9 +1,12 @@
 <? include('../conf/config.php');
 global $db;
-if(isset($_POST['round']) && isset($_POST['id'])) {
-	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_POST[id]");
+if(isset($_POST['MatchID'])) {
+	$match = $db->select("`Match`", "Round, EntryID1", "MatchID='$_POST[MatchID]'", "", "", "1");
+	$round = $match['Round'];
+	$id = $db->select("Entry", "TournamentID", "EntryID=$match[EntryID1]", "", "", "1")['TournamentID'];
+	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$id");
 	$initialEntries = merge($initialEntries, []);
-	if($_POST['round'] == '0') { // first round: gen from Entry
+	if($round == '0') { // first round: gen from Entry
 		$byes = pow(2, ceil(log(count($initialEntries), 2)))-count($initialEntries);
 		for($i=0;$i<$byes;$i++) { // award byes
 			array_splice($initialEntries, $i*2+1, 0, [-1]);
@@ -12,25 +15,25 @@ if(isset($_POST['round']) && isset($_POST['id'])) {
 	} else {
 		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 		"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-		AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND 
+		AND e.TournamentID = '$id' AND m.Round = $round-1 AND 
 		m.Result IS NULL")[0]['COUNT(*)'] == 0) { // all results in from previous round
-			if($_POST['round'] >= ceil(log(count($initialEntries), 2))) // final round
+			if($round >= ceil(log(count($initialEntries), 2))) // final round
 				$lastMatch = $db->select("`Match` as m , Entry as e", "EntryID1, EntryID2, Result",
 					"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-					e.TournamentID = '$_POST[id]' AND m.Round = '$_POST[round]'");
+					e.TournamentID = '$id' AND m.Round = '$round'");
 				$winnerID = $lastMatch[$lastMatch['Result'] == 'FIRST' ? 'EntryID1' : 'EntryID2'];
 				$winner = $Participant->getParticipantInfo($winnerID);
-				$db->update("Tournament", "Status='CLOSE', Winner='$winner'", "TournamentID='$_POST[id]'");
+				$db->update("Tournament", "Status='CLOSE', Winner='$winner'", "TournamentID='$id'");
 			else {
 				$first = $db->select("`Match` as m , Entry as e", "EntryID1",
 				"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-				AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND
+				AND e.TournamentID = '$id' AND m.Round = $round-1 AND
 				m.Result = 'FIRST'", "MatchID");
 				$second = $db->select("`Match` as m , Entry as e", "EntryID2",
 				"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-				AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND
+				AND e.TournamentID = '$id' AND m.Round = $round-1 AND
 				m.Result = 'SECOND'", "MatchID");
-				gen_matches($_POST['round'], merge($first, $second), $db );
+				gen_matches($round, merge($first, $second), $db );
 			}
 		}
 	}
