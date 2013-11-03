a7074c6c3d3f0ce89d4f099f000ecb470088e535
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 01:45:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function submitResult(m_id, res){
     alert(res);
     $.ajax({
-       url:'ajax/submit_match_result.php',
+       url:'http://masterme120.chronos.feralhosting.com/hackathon/dev/ajax/submit_match_result.php',
        type: 'POST',
        data:{MatchID : m_id , result : res },
        success: function (data){
