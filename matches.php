56b46778f80ee82f078383937a3ebcfc5db4d2ad
BenjaminFair <fair.105@osu.edu>
2013-11-02 21:53:14 -0400

    matches.php try 2

--- matches.php
+++ matches.php
@@ -1,12 +1,12 @@
 <? include("conf/config.php");
 include("inc/header.php");
-if(!isset($_GET[id])) {
+if(!isset($_GET["id"])) {
 	echo "Error: No tournament ID supplied";
 } else {
 	$result = $db->select("Tournament", "Name, Description",
 		"TournamentID = $_GET[id]")[0]; // get name and description
-	echo "<h1>Current Matches for " . $result[Name] . "</h1>";
-	echo "<p>" . $result[Description] . "</p>"; ?>
+	echo "<h1>Current Matches for " . $result["Name"] . "</h1>";
+	echo "<p>" . $result["Description"] . "</p>"; ?>
 <table>
 	<thead>
 		<tr>
@@ -16,18 +16,17 @@ if(!isset($_GET[id])) {
 		</tr>
 	</thead>
 	<tbody>
-<? $matches = $db->select("`Match`", "EntryID1, EntryID2, Result",
-		"TournamentID = $_GET[id]");
-	//foreach $matches as $match { 
-	$match = $matches[0]?>
+<? $matches = $db->select("`Match` as m , Entry as e", "EntryID1, EntryID2, Result",
+		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = 2");
+	foreach $matches as $match {?>
 		<tr>
 			<td><?echo $match[0]?></td>
 			<td><?echo $match[1]?></td>
-			<td><?	if($match[Result] == "FIRST") echo $match[0];
-					elseif($match[Result] == "SECOND") echo $match[1];
+			<td><?	if($match["Result"] == "FIRST") echo $match[0];
+					elseif($match["Result"] == "SECOND") echo $match[1];
 					else echo "Draw";?></td>
 		</tr>
-	<?//} // end foreach ?>
+	<?} // end foreach ?>
 	</tbody>
 </table>
 <?} include("inc/footer.php"); ?>
