bf51aec4bb8156973b71996c27a842a97f08032f
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 12:43:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function progressTournament(t_id){
 		type: 'POST',
 		data: {TournamentID :t_id },
 		success: function(data){
-			alert(data);
+			//alert(data);
 			location.reload(true);
 		/*	$.ajax({
 				url:'ajax/gen_match.php',
