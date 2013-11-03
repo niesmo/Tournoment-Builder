6e9b1486cc31224d00cf959e08dedcdfd1f2e779
BenjaminFair <fair.105@osu.edu>
2013-11-03 01:57:29 -0500

    debug 5

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,7 +1,10 @@
 <? include('../conf/config.php');
 if(isset($_GET['round']) && isset($_GET['id'])) {
 	echo "found data from request";
-} else echo "round and/or id not found in request!";
+} 
+else{ 
+	echo "round and/or id not found in request!";
+}
 
 /*private function merge($a, $b) {
 	$out = array();
@@ -11,7 +14,7 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 	return $out;
 }*/
 
-private function gen_matches($round, $entryIDs) {
+function gen_matches($round, $entryIDs) {
 	print_r($entryIDs);
 }
 ?>
