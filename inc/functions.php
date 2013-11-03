b328663199f4a6837e1fd8ad3e9855c6380e82a8
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 04:12:29 -0500

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -37,7 +37,7 @@ function includeHeader($title){
       
           <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
-              <div class="container-fluid">
+              <div class="container">
                 <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
