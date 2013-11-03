89220974fd47b4104e158cd5bd984c689718ba20
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 22:08:00 -0400

    blah blah

--- register.php
+++ register.php
@@ -14,10 +14,10 @@ if(!isset($_GET[id])) {
 	$result = $db->select("Tournament", "Name, Description, Rules",
 		"TournamentID = $_GET[id]")[0]; // get name, description, and rules
 	echo "<h1>Register for " . $result['Name'] . "</h1>"; // name
-	echo "<p>" . $result['Description'] . "</p>"; // description
-	echo "<h3>Rules:</h3><p>" . $result['Rules'] . "</p>";
+	echo "<p>" . $result[Description] . "</p>"; // description
+	echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
 
-	$participants =$tournament->getParticipants('$_GET[id]');
+	$participants =$tournament->getParticipants($_GET[id]);
 	// print_r($participants);
 
 	echo "<h3>Current Participants:</h3><ol>";
