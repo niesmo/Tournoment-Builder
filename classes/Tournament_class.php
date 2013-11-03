786a229869dc06f3c3ffb5af7e12a740ba31b562
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 10:19:18 -0500

    Update Tournament_class.php

--- classes/Tournament_class.php
+++ classes/Tournament_class.php
@@ -27,23 +27,35 @@ class Tournament{
         
         public function closeRegistration($tournament_id , $fields = "*"){
                 $this->db->update("Tournament" , "Status = 'PROGRESS'" , "TournamentID = $tournament_id");
-				$participants = $this->db->select("Tournament as t, Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.TournamentID = t.TournamentID AND e.TournamentID = $tournament_id");
-				$numberOfPartsNeeded = pow(2,ceil(log(count($participants) , 2)));
-				$count = count($participants) -1;
-				for($i=($numberOfPartsNeeded-1);$i>=0;$i--){
-					if($count < 0)
-						break;
-					echo " COUNT " . $count . "\n" ;
-					echo " I " . $i . "\n" ;
-					
-					if($i > count($participants)-1){
-						$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , -1 , 1 , 'FIRST' , 0");
-					}
-					else{
-						$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , " .$participants[$count--]['EntryID'] . " , -1 , NULL , 0");
-						$i--;
-					}
-				}
+		$participants = $this->db->select("Tournament as t, Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.TournamentID = t.TournamentID AND e.TournamentID = $tournament_id");
+		$numberOfPartsNeeded = pow(2,ceil(log(count($participants) , 2)));
+		$count = count($participants) -1;
+		for($i=($numberOfPartsNeeded-1);$i>=0;$i--){
+			if($count < 0)
+				break;
+			echo " COUNT " . $count . "\n" ;
+			echo " I " . $i . "\n" ;
+			
+			if($i > count($participants)-1){
+				$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , -1 , 1 , 'FIRST' , 0");
+			}
+			else{
+				$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , " .$participants[$count--]['EntryID'] . " , -1 , NULL , 0");
+				$i--;
+			}
+		}
         }
+        public function submitMatch($matchID, $result){
+        	
+        }
+        
+        public function getTournamentID($matchID){
+        	$entry  = $this->db->select("`Match as m`, Entry as e" , "EntryID" , "m.EntryID1 = e.EntryID" , "" , "", 1);
+        	$entry = $entry['EntryID'];
+        	
+        	$t_id = $this->db->select("Tournament as t, Entry as e", "e.TournamentID" , "t.TournamentID" , "" , "" , 1);
+        	return $t_id['TournamentID'];
+        }
+        
 }
 ?>
