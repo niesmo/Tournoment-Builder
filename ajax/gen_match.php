69286e3847115e2e655f3e201e8f4676e778563f
BenjaminFair <fair.105@osu.edu>
2013-11-03 02:44:11 -0500

    debug 1

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -35,9 +35,9 @@ function merge($a, $b) {
 }
 
 function gen_matches($round, $entryIDs) {
-	for($i=0;$i<count($entryIDs)/2;$i++) {
+	/*for($i=0;$i<count($entryIDs)/2;$i++) {
 		$db->insert("`Match`", "EntryID1, EntryID2, Round, Order", "'$entryIDs[i*2]', 
 		'$entryIDs[i*2+1]', '$round', '$i'");
-	}
+	}*/
 }
 ?>
