80a6adf052bea0176986dca8c8fcb3719ec17e6b
zschro <zack@dhcp-164-107-14-99.osuwireless.ohio-state.edu>
2013-11-02 21:30:06 -0400

    asdf

--- register.php
+++ register.php
@@ -17,6 +17,11 @@ if(!isset($_GET[id])) {
 	echo "<h1>Register for " . $result[Name] . "</h1>"; // name
 	echo "<p>" . $result[Description] . "</p>"; // description
 	echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
+
+	$participants =$tournament->getparticipants($_GET[id]);
+	print_r($participants);
+
+	echo "<h3>Current Participants:</h3><p>" . $participants[] . "</p>";
 ?>
 
 <form action="#" method="POST">
