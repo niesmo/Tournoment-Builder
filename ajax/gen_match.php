e00e4f01de6463669f600ded510c68d2fbbc8977
BenjaminFair <fair.105@osu.edu>
2013-11-03 01:59:59 -0500

    remove private keyword

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,13 +1,32 @@
 <? include('../conf/config.php');
 if(isset($_GET['round']) && isset($_GET['id'])) {
-	echo "found data from request";
-} 
-else{ 
-	echo "round and/or id not found in request!";
-}
+	$initialEntries = $db->select("Entry", "COUNT(*)", "TournamentID=$_GET[id]")[0][0];
+	if($_GET['round'] == '0') { // first round: gen from Entry
+		gen_matches(0, merge($initialEntries, []));
+	} else {
+		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
+		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
+		e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
+		m.Result != 'null'", "MatchID")[0][0] == 0) { // all results in from previous round
+			if($round >= ceil(log(count($initialEntries, 2))) // final round
+				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
+			else {
+				$first = db->select("`Match` as m , Entry as e", "EntryID1",
+				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				m.Result = 'FIRST'", "MatchID");
+				$second = db->select("`Match` as m , Entry as e", "EntryID2",
+				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
+				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				m.Result = 'SECOND'", "MatchID");
+				gen_matches($_GET['round'], merge($first, $second)));
+			}
+		}
+	}
+} else echo "round and/or id not found in request!";
 
 function merge($a, $b) {
-	$out = array();
+	$out = [];
 	foreach($a as $array) foreach($array as $key => $value)	$out[] = $value;
 	foreach($b as $array) foreach($array as $key => $value)	$out[] = $value;
 	sort($out);
