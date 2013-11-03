f858816c2c2c52fd4b746daee399de27dbfe7854
BenjaminFair <fair.105@osu.edu>
2013-11-02 21:26:54 -0400

    Add one match back

--- matches.php
+++ matches.php
@@ -16,11 +16,18 @@ if(!isset($_GET[id])) {
 		</tr>
 	</thead>
 	<tbody>
+<? $matches = $db->select("`Match`", "EntryID1, EntryID2, Result",
+		"TournamentID = $_GET[id]");
+	//foreach $matches as $match { 
+	$match = $matches[0]?>
 		<tr>
-			<td>Test1</td>
-			<td>Test2</td>
-			<td>Tie</td>
+			<td><?echo $match[0]?></td>
+			<td><?echo $match[1]?></td>
+			<td><?	if($match[Result] == "FIRST") echo $match[0];
+					elseif($match[Result] == "SECOND") echo $match[1];
+					else echo "Draw";?></td>
 		</tr>
+	<?//} // end foreach ?>
 	</tbody>
 </table>
 <?} include("inc/footer.php"); ?>
