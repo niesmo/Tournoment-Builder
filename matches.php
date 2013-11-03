f152988486eab6e115dfa66b399da5be5737e0f2
BenjaminFair <fair.105@osu.edu>
2013-11-02 20:59:50 -0400

    Preliminary matches.php

---
+++ matches.php
@@ -0,0 +1,32 @@
+<? include("conf/config.php");
+include("inc/header.php");
+if(!isset($_GET[id])) {
+	echo "Error: No tournament ID supplied";
+} else {
+	$result = $db->select("Tournament", "Name, Description",
+		"TournamentID = $_GET[id]")[0]; // get name and description
+	echo "<h1>Current Matches for " . $result[Name] . "</h1>";
+	echo "<p>" . $result[Description] . "</p>"; ?>
+<table>
+	<thead>
+		<tr>
+			<th>Entrant 1</th>
+			<th>Entrant 2</th>
+			<th>Winner</th>
+		</tr>
+	</thead>
+	<tbody>
+<? $matches = $db->select("`Match`", "EntryID1, EntryID2, Result",
+		"TournamentID = $_GET[id]");
+	foreach $matches as $match { ?>
+		<tr>
+			<td><?echo $match[0]?></td>
+			<td><?echo $match[1]?></td>
+			<td><?	if($match[Result] == "FIRST") echo $match[0];
+					elseif($match[Result] == "SECOND") echo $match[1];
+					else echo "Draw";?></td>
+		</tr>
+	<?} // end foreach ?>
+	</tbody>
+</table>
+<? include("inc/footer.php"); ?>
