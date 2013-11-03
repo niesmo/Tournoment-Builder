92938ec3dc61644690acf6544af61545894a8b1d
BenjaminFair <fair.105@osu.edu>
2013-11-03 02:01:15 -0500

    debug 1

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -8,19 +8,7 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
 		e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
 		m.Result != 'null'", "MatchID")[0][0] == 0) { // all results in from previous round
-			if($round >= ceil(log(count($initialEntries, 2))) // final round
-				db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
-			else {
-				$first = db->select("`Match` as m , Entry as e", "EntryID1",
-				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
-				m.Result = 'FIRST'", "MatchID");
-				$second = db->select("`Match` as m , Entry as e", "EntryID2",
-				"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND 
-				e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
-				m.Result = 'SECOND'", "MatchID");
-				gen_matches($_GET['round'], merge($first, $second)));
-			}
+
 		}
 	}
 } else echo "round and/or id not found in request!";
