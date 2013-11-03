3c2a40b1f90b5b611b62ea8a0b51da95102d691f
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 21:19:28 -0400

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,64 @@
 <?
 function includeHeader($title){
-
+      echo '<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>'.$title.'</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="description" content="">
+          <meta name="author" content="">
+      
+          <!-- Le styles -->
+          <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/bootstrap.css" rel="stylesheet">
+          <style type="text/css">
+            body {
+              padding-top: 60px;
+              padding-bottom: 40px;
+            }
+            .sidebar-nav {
+              padding: 9px 0;
+            }
+      
+            @media (max-width: 980px) {
+              /* Enable use of floated navbar text */
+              .navbar-text.pull-right {
+                float: none;
+                padding-left: 5px;
+                padding-right: 5px;
+              }
+            }
+          </style>
+          <link href="http://masterme120.chronos.feralhosting.com/hackathon/dev/css/bootstrap-responsive.css" rel="stylesheet">
+      
+          </head>
+      
+        <body>
+      
+          <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+              <div class="container-fluid">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="brand" href="http://masterme120.chronos.feralhosting.com/hackathon/dev/">Tournament Builder</a>
+                <div class="nav-collapse collapse">
+                  <p class="navbar-text pull-right">
+                    Logged in as <a href="#" class="navbar-link">Username</a>
+                  </p>
+                  <ul class="nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                  </ul>
+                </div><!--/.nav-collapse -->
+              </div>
+            </div>
+          </div>
+      
+          <div class="container-fluid">
+            <div class="row-fluid">';
 }
 ?>
