5fe381566ebf9c3de2ec3190d9efab9617d42de2
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 00:56:36 -0400

    Update matches.php

--- matches.php
+++ matches.php
@@ -7,42 +7,44 @@ if(!isset($_GET["id"])) {
 		"TournamentID = $_GET[id]")[0]; // get name and description
 	echo "<h1>Current Matches for " . $result["Name"] . "</h1>";
 	echo "<p>" . $result["Description"] . "</p>"; ?>
-<table>
-	<thead>
-		<tr>
-			<th>Entrant 1</th>
-			<th>Entrant 2</th>
-			<th>Winner</th>
-			<th></th>
-		</tr>
-	</thead>
-	<tbody>
-<? 	$matches = $db->select("`Match` as m , Entry as e, Participant as p", "EntryID1, EntryID2, Result, p.Name",
-	"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = '$_GET[id]'",
-	"MatchID");
-foreach($matches as $match) {?>
-		<tr>
-		<?if($match["Result"] == "") { // match in progress
-			$part_1 = $participant->getParticipantInfo($match['EntryID1']);
-			$part_2 = $participant->getParticipantInfo($match['EntryID2']);
-			print_r( $part_1);
-			print_r( $part_2);
-		?>
-			//getting the participant info
-			<td><button class="btn"><?echo $part_1['Name']?></button></td>
-			<td><button class="btn"><?echo $part_2['Name']?></button></td>
-			<td><i>Awaiting Results</i></td>
-			<td><button class="btn">Draw</button>
-		<?} else {?>
-			<td><?echo $match["EntryID1"]?></td>
-			<td><?echo $match["EntryID2"]?></td>
-			<td><?	if($match["Result"] == "FIRST") echo $part_1['Name'];
-					elseif($match["Result"] == "SECOND") echo $part_2['Name'];
-					elseif($match["Result"] == "TIE") echo "Draw";?></td>
-			<td></td>
-		<?}?>
-		</tr>
-<?} // end foreach ?>
-	</tbody>
-</table>
-<?} include("inc/footer.php"); ?>
+	<table>
+		<thead>
+			<tr>
+				<th>Entrant 1</th>
+				<th>Entrant 2</th>
+				<th>Winner</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+	<? 	$matches = $db->select("`Match` as m , Entry as e, Participant as p", "EntryID1, EntryID2, Result, p.Name",
+		"(e.EntryID = m.EntryID1 OR e.EntryID = m.EntryID2) AND e.TournamentID = '$_GET[id]'",
+		"MatchID");
+	foreach($matches as $match) {?>
+			<tr>
+			<?if($match["Result"] == "") { // match in progress
+				echo $match['EntryID1'];
+				$part_1 = $participant->getParticipantInfo($match['EntryID1']);
+				$part_2 = $participant->getParticipantInfo($match['EntryID2']);
+				print_r( $part_1);
+				print_r( $part_2);
+			?>
+				//getting the participant info
+				<td><button class="btn"><?echo $part_1['Name']?></button></td>
+				<td><button class="btn"><?echo $part_2['Name']?></button></td>
+				<td><i>Awaiting Results</i></td>
+				<td><button class="btn">Draw</button>
+			<?} else {?>
+				<td><?echo $match["EntryID1"]?></td>
+				<td><?echo $match["EntryID2"]?></td>
+				<td><?	if($match["Result"] == "FIRST") echo $part_1['Name'];
+						elseif($match["Result"] == "SECOND") echo $part_2['Name'];
+						elseif($match["Result"] == "TIE") echo "Draw";?></td>
+				<td></td>
+			<?}?>
+			</tr>
+	<?} // end foreach ?>
+		</tbody>
+	</table>
+<?}
+include("inc/footer.php"); ?>
