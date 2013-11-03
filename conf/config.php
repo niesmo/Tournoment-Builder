e9b52120832821efd94275f0d6d13583fe13f97f
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 21:50:43 -0400

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -16,7 +16,7 @@ define ( "DB_PASS", "1yy5u8Uwmy1go57h" );
 define ( "DB_HOST", "localhost" );
 define ( "DB_DB", "Tournament" );
 
-$db = new Database ( ${DB_HOST}, ${DB_USER}, ${DB_PASS}, ${DB_DB} );
+$db = new Database ( DB_HOST, DB_USER, DB_PASS, DB_DB );
 if (!$db->isConnected())
     die("DATABASE IS NOT CONNECTED");
 //else
