b2f923029980ea17162bcbf5755486143ed49dc8
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 09:15:19 -0500

    Update Tournament_class.php

--- classes/Tournament_class.php
+++ classes/Tournament_class.php
@@ -1,32 +1,44 @@
 <?
 class Tournament{
-	private $db;
-	
-	public function __construct( $db ){
-		$this->db = $db;
-	}
-	
-	public function newTournament($name, $type, $description, $rules, $maxPlayer){
-		$now = date("Y-m-d H:i:s"); 
-	        if($this->db->insert("Tournament", "Name, Type, StartDate, Status, Description, Rules, MaxPlayer",
-	                "'$name', '$type', '$now', 'OPEN', '$description', '$rules' , $maxPlayer") == 1)
-	        	return $this->db->lastInsertedId();
-	        else
-	        	return -1;
-	}
-	
-	public function getParticipants($tournament_id){
-		return $this->db->select("Entry as e, Participant as p" , "*" , "e.TournamentID = $tournament_id AND e.ParticipantID = p.ParticipantID");
-	}
-	
-	public function getMatches($tournament_id, $field="*") {
-		return $this->db->select("`Match` as m , Entry as e", $field,
-		"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-		AND e.TournamentID = '$tournament_id'", "MatchID");
-	}
-	
-	public function closeRegistration($tournament_id){
-		$this->db->update("Tournament" , "Status = 'PROGRESS'" , "TournamentID = $tournament_id");
-	}
+        private $db;
+        
+        public function __construct( $db ){
+                $this->db = $db;
+        }
+        
+        public function newTournament($name, $type, $description, $rules, $maxPlayer){
+                $now = date("Y-m-d H:i:s"); 
+                if($this->db->insert("Tournament", "Name, Type, StartDate, Status, Description, Rules, MaxPlayer",
+                        "'$name', '$type', '$now', 'OPEN', '$description', '$rules' , $maxPlayer") == 1)
+                        return $this->db->lastInsertedId();
+                else
+                        return -1;
+        }
+        
+        public function getParticipants($tournament_id){
+                return $this->db->select("Entry as e, Participant as p" , "*" , "e.TournamentID = $tournament_id AND e.ParticipantID = p.ParticipantID");
+        }
+        
+        public function getMatches($tournament_id, $field="*") {
+                return $this->db->select("`Match` as m , Entry as e", $field,
+                "(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
+                AND e.TournamentID = '$tournament_id'", "MatchID");
+        }
+        
+        public function closeRegistration($tournament_id , $fields = "*"){
+                $this->db->update("Tournament" , "Status = 'PROGRESS'" , "TournamentID = $tournament_id");
+				$participants = $this->db->select("Tournament as t, Entry as e, Participant as p" , "$fields" , "e.ParticipantID = p.ParticipantID AND e.TournamentID = t.TournamentID");
+				$numberOfPartsNeeded = pow(2,ceil(log(count($participants) , 2)));
+				$count = ($numberOfPartsNeeded-1);
+				for($i=($numberOfPartsNeeded-1);$i>=0;$i--){
+					if($i > count($participants)-1){
+						$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , -1 , 1 , 'FIRST' , 0");
+					}
+					else{
+						$this->db->insert("`Match`" , "EntryID1, EntryID2, Bye, Result, Round" , $participants[$count--]['EntryID'] . " , " .$participants[$count--]['EntryID'] . " , -1 , NULL , 0");
+						$i--;
+					}
+				}
+        }
 }
 ?>
