4f422c6b3e1a756459a4abc9b4939b7cc28e45b4
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 01:18:35 -0500

    Update submit_match_result.php

--- ajax/submit_match_result.php
+++ ajax/submit_match_result.php
@@ -1,7 +1,4 @@
 <?include("../conf/config.php");
-if($db->isConnected()){
-	echo "Connected";
-}
 if(isset($_POST["result"]) && isset($_POST["MatchID")) {
 	print_r($_POST);
 	//if($db->update("`Match`", "Result = '$_POST[result]'", " MatchID = $_POST[MatchID]") == 1)
