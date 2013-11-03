b63bc9a18f0a23a2a2514ecf1592d7acaaa64eb1
BenjaminFair <fair.105@osu.edu>
2013-11-03 05:27:01 -0500

    change from GET to POST

--- ajax/gen_match.php
+++ ajax/gen_match.php
@@ -1,9 +1,9 @@
 <? include('../conf/config.php');
 global $db;
-if(isset($_GET['round']) && isset($_GET['id'])) {
-	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_GET[id]");
+if(isset($_POST['round']) && isset($_POST['id'])) {
+	$initialEntries = $db->select("Entry", "EntryID", "TournamentID=$_POST[id]");
 	$initialEntries = merge($initialEntries, []);
-	if($_GET['round'] == '0') { // first round: gen from Entry
+	if($_POST['round'] == '0') { // first round: gen from Entry
 		$byes = pow(2, ceil(log(count($initialEntries), 2)))-count($initialEntries);
 		for($i=0;$i<$byes;$i++) { // award byes
 			array_splice($initialEntries, $i*2+1, 0, [-1]);
@@ -12,20 +12,20 @@ if(isset($_GET['round']) && isset($_GET['id'])) {
 	} else {
 		if($db->select("`Match` as m , Entry as e", "COUNT(*)",
 		"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-		AND e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND 
+		AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND 
 		m.Result IS NULL")[0]['COUNT(*)'] == 0) { // all results in from previous round
-			if($_GET['round'] >= ceil(log(count($initialEntries), 2))) // final round
-				$db->update("Tournament", "Status='CLOSE'", "TournamentID='$_GET[id]'");
+			if($_POST['round'] >= ceil(log(count($initialEntries), 2))) // final round
+				$db->update("Tournament", "Status='CLOSE'", "TournamentID='$_POST[id]'");
 			else {
 				$first = $db->select("`Match` as m , Entry as e", "EntryID1",
 				"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-				AND e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND
 				m.Result = 'FIRST'", "MatchID");
 				$second = $db->select("`Match` as m , Entry as e", "EntryID2",
 				"(e.EntryID = m.EntryID1 OR (e.EntryID = m.EntryID2 OR m.EntryID2 = -1)) 
-				AND e.TournamentID = '$_GET[id]' AND m.Round = $_GET[round]-1 AND
+				AND e.TournamentID = '$_POST[id]' AND m.Round = $_POST[round]-1 AND
 				m.Result = 'SECOND'", "MatchID");
-				gen_matches($_GET['round'], merge($first, $second), $db );
+				gen_matches($_POST['round'], merge($first, $second), $db );
 			}
 		}
 	}
