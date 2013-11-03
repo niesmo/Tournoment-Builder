6893f263f4b5347edfbe240dd8bfdb25de3ef6b9
BenjaminFair <fair.105@osu.edu>
2013-11-02 20:06:56 -0400

    Add debug

--- register.php
+++ register.php
@@ -9,7 +9,8 @@ if(!isset($_GET[id])) {
 		$db->insert("Entry", "TournamentID, ParticipantID", "'$_GET[id]', '$Id'");
 	}
 	$result = $db->select("Tournament", "Name, Description, Rules",
-		"TournamentID = '$_GET[id]'", "", "", "1"); // get name, description, and rules
+		"TournamentID = $_GET[id]"); // get name, description, and rules
+	print_r($result);
 	echo "<h1>Register for " . $result[Name] . "</h1>"; // name
 	echo "<p>" . $result[Description] . "</p>"; // description
 	echo "<h3>Rules:</h3><p>" . $result[Rules] . "</p>";
