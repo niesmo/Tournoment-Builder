a5c48a1a4190ca03f8fc9b98c6dd2d6d447331e9
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-03 04:49:10 -0500

    Update register.php

--- register.php
+++ register.php
@@ -39,7 +39,7 @@ if(!isset($_GET['id']) && !isset($_POST['id'])) {
 }
 ?>
 
-<?if($result[MaxPlayer] < count($participants)){?>
+<?if($result['MaxPlayer'] < count($participants)){?>
 <form action="register.php" method="POST">
   <fieldset>
     <h2 class="form-signin-heading">Register for this tournament</h2>
