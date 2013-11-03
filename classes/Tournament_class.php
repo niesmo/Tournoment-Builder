d2ca3cbfeb163218f5b504eb39af7c446d973934
osuHackathon <esmailimokaram.1@osu.edu>
2013-11-02 21:07:07 -0400

    Update Tournament_class.php

--- classes/Tournament_class.php
+++ classes/Tournament_class.php
@@ -9,6 +9,7 @@ class Tournament{
 	
 	public function newTournament($name, $type, $description, $rules){
 		$now = date("Y-m-d H:i:s"); 
+		echo "test";
 	        return $this->db->insert("Tournament", "Name, Type, StartDate, Status, Description, Rules",
 	                "'$name', '$type', '$now', 'OPEN', '$description', '$rules'");
 	}
